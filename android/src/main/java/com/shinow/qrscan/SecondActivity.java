9f3137923b2a5838cb7a123a855329a1f116b93d
Torrunt <me@torrunt.net>
2020-04-09 16:39:43 +0800

    Screen rotate fix attempt (cont.)

--- android/src/main/java/com/shinow/qrscan/SecondActivity.java
+++ android/src/main/java/com/shinow/qrscan/SecondActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.content.pm.ActivityInfo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
